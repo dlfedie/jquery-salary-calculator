3017fbc64b4dd0942bf90e97063cd6b1b2d2bf6a
Dustin Fedie <dlfedie@gmail.com>
2019-07-26 16:56:42 -0500

    Starting on JS

--- client.js
+++ client.js
@@ -1,6 +1,18 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('JQ YO');
-    
+    //event listeners
+    $('#submitButton').on('click', submitButton);
+}
+
+function submitButton() {
+    console.log('Clicked submit');
+    $('#tableBody').append(`<tr>
+                    <td>Dust</td>
+                    <td>Fedie</td>
+                    <td>1234</td>
+                    <td>Dude</td>
+                    <td>$1,000,000</td>
+                    <td><button>Delete</button></td>
+                </tr>`)
 }
